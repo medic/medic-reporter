22e1b7165415611f948a8b13321455a6ebcbf810
Milan Andric <mandric@gmail.com>
2013-02-14 00:20:44 -0600

    updated require via jam.

--- jam/require.config.js
+++ jam/require.config.js
@@ -60,7 +60,7 @@ var jam = {
             "main": "underscore.js"
         }
     ],
-    "version": "0.2.11",
+    "version": "0.2.13",
     "shim": {
         "director": {
             "exports": "Router"
@@ -69,10 +69,144 @@ var jam = {
 };
 
 if (typeof require !== "undefined" && require.config) {
-    require.config({packages: jam.packages, shim: jam.shim});
+    require.config({
+    "packages": [
+        {
+            "name": "bootstrap",
+            "location": "jam/bootstrap"
+        },
+        {
+            "name": "codemirror",
+            "location": "jam/codemirror",
+            "main": "lib/codemirror.js"
+        },
+        {
+            "name": "director",
+            "location": "jam/director",
+            "main": "director.js"
+        },
+        {
+            "name": "domReady",
+            "location": "jam/domReady",
+            "main": "domReady.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "jquery.lego",
+            "location": "jam/jquery.lego",
+            "main": "jquery.lego.js"
+        },
+        {
+            "name": "json",
+            "location": "jam/json",
+            "main": "json.js"
+        },
+        {
+            "name": "json.edit",
+            "location": "jam/json.edit",
+            "main": "json.edit.js"
+        },
+        {
+            "name": "schema-support",
+            "location": "jam/schema-support",
+            "main": "translator.js"
+        },
+        {
+            "name": "select2",
+            "location": "jam/select2",
+            "main": "select2.js"
+        },
+        {
+            "name": "text",
+            "location": "jam/text",
+            "main": "text.js"
+        },
+        {
+            "name": "underscore",
+            "location": "jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "shim": {
+        "director": {
+            "exports": "Router"
+        }
+    }
+});
 }
 else {
-    var require = {packages: jam.packages, shim: jam.shim};
+    var require = {
+    "packages": [
+        {
+            "name": "bootstrap",
+            "location": "jam/bootstrap"
+        },
+        {
+            "name": "codemirror",
+            "location": "jam/codemirror",
+            "main": "lib/codemirror.js"
+        },
+        {
+            "name": "director",
+            "location": "jam/director",
+            "main": "director.js"
+        },
+        {
+            "name": "domReady",
+            "location": "jam/domReady",
+            "main": "domReady.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "jquery.lego",
+            "location": "jam/jquery.lego",
+            "main": "jquery.lego.js"
+        },
+        {
+            "name": "json",
+            "location": "jam/json",
+            "main": "json.js"
+        },
+        {
+            "name": "json.edit",
+            "location": "jam/json.edit",
+            "main": "json.edit.js"
+        },
+        {
+            "name": "schema-support",
+            "location": "jam/schema-support",
+            "main": "translator.js"
+        },
+        {
+            "name": "select2",
+            "location": "jam/select2",
+            "main": "select2.js"
+        },
+        {
+            "name": "text",
+            "location": "jam/text",
+            "main": "text.js"
+        },
+        {
+            "name": "underscore",
+            "location": "jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "shim": {
+        "director": {
+            "exports": "Router"
+        }
+    }
+};
 }
 
 if (typeof exports !== "undefined" && typeof module !== "undefined") {
