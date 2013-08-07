655d36b3af12657c5878abf2c239d78a6424d744
Ryan Ramage <ryan.ramage@gmail.com>
2013-08-07 13:10:36 -0600

    Fixes #1.

--- jam/schema-support/translator.js
+++ jam/schema-support/translator.js
@@ -1,13 +1,77 @@
 
 (function (root, factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
+
+    if (typeof define === 'function' && define.amd) {
         define(['./src/validations'],factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
     } else {
         root.returnExports = factory(root.validations);
     }
 }(this, function (validations) {
+
+    if (!validations) {
+         validations =  {
+
+            result: function(ok, error) {
+                var response = {
+                    ok : ok
+                }
+                if (!ok) {
+                    response.error = error
+                }
+                return response;
+            },
+            is_numeric_year : function(value) {
+                if (value >= 2011 && value <= 2099) return validations.result(true);
+                return validations.result(false, {
+                    en : "Year must be between 2011 and 2099",
+                    fr : "l'ann\5e doit etre entre 2011 et 2099"
+                });
+            },
+            is_numeric_month : function(value) {
+                if (value >= 1 && value <= 12) {
+                    return validations.result(true);
+                }
+                return validations.result(false, {
+                    en : "Month must be between 1 and 12",
+                    fr : "le mois doit etre entre 1 et 12"
+                });
+            },
+            is_numeric_day: function(value) {
+                if (value >= 1 && value <= 31) {
+                    return validations.result(true);
+                }
+                return validations.result(false, {
+                    en : "Day must be between 1 and 31",
+                    fr : "Le jour doit etre entre 1 et 31"
+                });
+            },
+            is_valid_time: function(value) {
+                if (value >= 0 && value <= 2359 &&
+                            value % 100 >= 0 && value % 100 <= 59) return validations.result(true);
+                return validations.result(false, {
+                    en : "Hours must be bewteen 0000 and 2359",
+                    fr : "L'heure doit etre entre 0000 et 2359"
+                });
+            },
+            is_numeric_daysago: function(value) {
+                return validations.result(true);
+            },
+            is_numeric_district: function(value) {
+                if (value >= 1 && value <= 20) {
+                    return validations.result(true);
+                }
+                return validations.result(false, {
+                    en : "District must be between 1 and 20",
+                    fr : "le district de sant\5 doit etre entre 1 et 20"
+                });
+            }
+        };
+    }
+
+
+
 
     return function(form, lang) {
         var result = {};
